ee363e42c470ef49f7c83b8f0b6d2e736136b4c8
YessineLajmi <yassinlajmi6@gmail.com>
2023-08-15 15:10:17 +0100

    first jsx

---
+++ first-tray/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello Coding codjo</h1>
+      <h3>Things i need to do :</h3>
+      <ul>
+        <li>learn react</li>
+        <li>climb Mt.Everst</li>
+        <li>Run a marthone </li>
+        <li>Feed the dojo </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;

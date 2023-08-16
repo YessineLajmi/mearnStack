6622f9db5f060dee677688f8b40e384c20c4c744
YessineLajmi <yassinlajmi6@gmail.com>
2023-08-16 15:14:57 +0100

    last up

--- week 2/day1/prop-id-app/src/component/PersonCard.jsx
+++ week 2/day1/prop-id-app/src/component/PersonCard.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PersonCard = (props) => {
-  const { firstName, lastName, age, hairColor } = props;
+  const { firstName, lastName, hairColor } = props;
+  const [age, setAge] = useState(0);
+
   return (
     <div>
       <h2>
         {lastName},{firstName}
         <p>age:{age}</p>
         <p>hairColor:{hairColor}</p>
+        <button onClick={(Event) => setAge(age + 1)}>
+          Birthday Button for Jane Doe
+        </button>
       </h2>
     </div>
   );

8f73f0453360c1a06f30f087cc773de84228184b
YessineLajmi <yassinlajmi6@gmail.com>
2023-08-24 12:27:27 +0100

    faker ipa

--- week 3/day 1/faker-api/server.js
+++ week 3/day 1/faker-api/server.js
@@ -7,6 +7,39 @@ const server = app.listen(port, () => {
   console.log(`this serveur is working right now ${port}`);
 });
 
-app.get("/api", (req, res) => {
-  res.send("hello /api is work thats great ");
-});
+// app.get("/api", (req, res) => {
+//   res.send("hello /api is work thats great ");
+// });
+// we can create a function to return a random / fake "Product"
+const user = () => {
+  const newFake = {
+    firstName: faker.person.fullName({ firstName: "Joann" }),
+    lastName: faker.person.fullName({ lastName: "Beer" }),
+    password: faker.internet.password(),
+    email: faker.email.text("joannBeer@faker.com"),
+    phoneNumber: faker.phone.imei("+16355478965"),
+    id: faker.id.number(55),
+  };
+  const url = app.listen("/api/user/new", (req, res) => {
+    return newFakeProduct;
+  });
+  return newFake;
+};
+const company = () => {
+  const newCompany = {
+    _id: faker.id.number(100),
+    name: faker.company.name("dojo"),
+    address: faker.location.streetAddress("tchno pol ghzela"),
+  };
+  return newCompany;
+};
+const newFakeProduct = createProduct();
+console.log(newFakeProduct);
+/*
+ * The output of the above console log will look like this
+ * {
+ *   name: 'Anime Figure',
+ *   price: '$568.00
+ *   department: 'Tools'
+ * }
+ */
